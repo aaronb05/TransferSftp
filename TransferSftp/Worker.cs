ea66fea67e5f04e460c0392c7900206fd7117e43
aaronb05 <aboyles05@gmail.com>
2022-06-06 16:08:45 -0400

    added change to csv method/added another watcher/added a copy to sent folder for archive purposes

--- TransferSftp/Worker.cs
+++ TransferSftp/Worker.cs
@@ -1,3 +1,6 @@
+using GroupDocs.Conversion;
+using GroupDocs.Conversion.FileTypes;
+using GroupDocs.Conversion.Options.Convert;
 using System;
 using System.IO;
 
@@ -38,13 +41,21 @@ namespace TransferSftp
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
-                using var watcher = new FileSystemWatcher(@"C:\TestFolder\sftp_client1\incoming");
-                watcher.EnableRaisingEvents = true;
-                watcher.NotifyFilter = NotifyFilters.CreationTime |
+                using var incomingWatcher = new FileSystemWatcher(@"C:\TestFolder\sftp_client1\incoming");
+                incomingWatcher.EnableRaisingEvents = true;
+                incomingWatcher.NotifyFilter = NotifyFilters.CreationTime |
                                        NotifyFilters.FileName |
                                        NotifyFilters.Size;
-                watcher.Created += OnCreated;
-                watcher.Filter = "*.txt";
+                incomingWatcher.Created += OnCreated;
+                //incomingWatcher.Filter = "*.xlsx";
+
+                using var stagingWatcher = new FileSystemWatcher(@"C:\TestFolder\sftp_client1\staging");
+                stagingWatcher.EnableRaisingEvents = true;
+                stagingWatcher.NotifyFilter = NotifyFilters.CreationTime |
+                                       NotifyFilters.FileName |
+                                       NotifyFilters.Size;
+                stagingWatcher.Created += ConvertToCsv;
+                //stagingWatcher.Filter = "*.xlsx";
 
                 await Task.Delay(5000, stoppingToken);
             }
@@ -52,12 +63,11 @@ namespace TransferSftp
 
 
         private static void OnCreated(object sender, FileSystemEventArgs e)
-        {           
-            //_logger.LogInformation("File " + e.Name + " created at: " + DateTime.Now.ToString());
-          
+        {          
+            //_logger.LogInformation("File " + e.Name + " created at: " + DateTime.Now.ToString());          
             //var fileName = e.Name;
-            var sourcePath = "C:\\TestFolder\\sftp_client1\\incoming";
-            var targetPath = "C:\\TestFolder\\sftp_client1\\to_send";
+            var sourcePath = @"C:\TestFolder\sftp_client1\incoming";
+            var targetPath = @"C:\TestFolder\sftp_client1\staging";
 
             if (Directory.Exists(targetPath))
             {
@@ -66,14 +76,46 @@ namespace TransferSftp
                 foreach (string s in files)
                 {
                     var date = DateTime.Today.ToString("dd-MM-yyyy");
-                    var sendFileName = "to_send_to_ftp" + date;
-                    Console.WriteLine(date);
-                    var destFile = Path.Combine(targetPath, sendFileName);
+                    var fileName = $"to_send_to_ftp_{date}.xlsx" ;
+                    var sendFile = Path.Combine(targetPath, fileName);
                     //_logger.LogInformation("File " + sendFileName + " has been moved to: " + targetPath);
-
-                    File.Move(s, destFile, true);
+                    
+                    File.Move(s, sendFile, true);
                 }
             }          
         }
+
+        private static void ConvertToCsv(object sender, FileSystemEventArgs e)
+        {
+            var date = DateTime.Today.ToString("dd-MM-yyyy");
+            var sourcePath = @"C:\TestFolder\sftp_client1\staging";
+            var targetPath = @$"C:\TestFolder\sftp_client1\to_send\converted_file_{date}.csv";
+            var copyFilePath = @"C:\TestFolder\sftp_client1\sent";
+
+            string[] files = Directory.GetFiles(sourcePath);
+
+            foreach(string s in files)
+            {
+                using (Converter converter = new Converter(s))
+                {
+                    SpreadsheetConvertOptions options = new SpreadsheetConvertOptions
+                    {
+                        PageNumber = 1,
+                        PagesCount = 1,
+                        Format = SpreadsheetFileType.Csv // Specify the conversion format
+                    };
+
+                    converter.Convert(targetPath, options);
+                }
+
+                
+                var fileName = $"sent_{date}.csv";
+                var copyFile = Path.Combine(copyFilePath, fileName);
+                //_logger.LogInformation("File " + sendFileName + " has been moved to: " + targetPath);
+
+                File.Copy(s, copyFile, true);
+                File.Move(s, targetPath, true);                
+            }
+        }     
     }
 }
